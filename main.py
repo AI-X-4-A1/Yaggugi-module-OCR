2c24c32fa1d5733f2e274bca3d2282a23789ea4c
DESKTOP-FMA1TJP\hi <wltkqdl@naver.com>
2024-10-29 15:41:07 +0900

    feat: fastapi 및 ocr 처리 임시 성공 1. fastapi 세팅 완료 2. ocr 영.한 모델 탑재 및 ocr 처리 성공 3. 후처리 일부 성공(전처리는 추후 업데이트 예정)

---
+++ main.py
@@ -0,0 +1,35 @@
+from typing import List
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+from paddleocr import PaddleOCR, draw_ocr # main OCR dependencies
+from utils import *
+
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True' #이거 안해주면 오류남
+
+# Setup model
+ocr_en_model = PaddleOCR(lang='en')
+ocr_kr_model = PaddleOCR(lang='korean')
+
+app = FastAPI()
+
+@app.post("/ocr/")
+async def create_upload_files(file: UploadFile):
+    content = await file.read()
+
+    ocr_texts = []              # 순서 유지
+    ocr_unique_texts = set()    # 중복 체크
+
+    # preprocess
+    prepreprocess_text()
+
+    # ocr
+    get_ocr_result(content, ocr_texts, ocr_unique_texts, ocr_en_model)  # en
+    get_ocr_result(content, ocr_texts, ocr_unique_texts, ocr_kr_model)  # ko
+
+    # postpreprocess
+    ocr_pp_unique_text = postpreprocess_text(ocr_texts, ocr_unique_texts)
+    print(f'ocr_pp_unique_text: {ocr_pp_unique_text}')
+
+    return {"ocr_result": ocr_pp_unique_text}
